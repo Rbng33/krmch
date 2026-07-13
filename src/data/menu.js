d9277a74fb1ea32d54c05d8b080dc6f778f5c91a
Rbng33 <la_benghanem@esi.com>
2026-07-13 21:13:02 +0100

    pngs

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,18 +1,18 @@
 export const categories = [
   { en: 'KABSA', ar: 'كبسة', img: '/kabsa.png' },
-  { en: 'BROAST', ar: 'بروست', img: '../public/broast.png' },
-  { en: 'BURGERS', ar: 'برغر', img: '../public/burgers.png' },
-  { en: 'ZINGER', ar: 'زنجر', img: '../public/zinger.png' },
-  { en: 'FRIES', ar: 'بطاطا', img: '../public/fries.png' },
-  { en: 'KUNAFA', ar: 'كنافة', img: '../public/kunafa.png' },
+  { en: 'BROAST', ar: 'بروست', img: '/broast.png' },
+  { en: 'BURGERS', ar: 'برغر', img: '/burgers.png' },
+  { en: 'ZINGER', ar: 'زنجر', img: '/zinger.png' },
+  { en: 'FRIES', ar: 'بطاطا', img: '/fries.png' },
+  { en: 'KUNAFA', ar: 'كنافة', img: '/kunafa.png' },
 ]
 
 export const bestSellers = [
-  { tag: 'ZINGER', title: 'Zinger Poulet', price: '350 DA', img: '../public/m6.png' },
-  { tag: 'BROAST', title: '2 Pcs Chicken + Fries', price: '450 DA', img: '../public/m1.png' },
-  { tag: 'BROAST', title: '4 Pcs Chicken + Fries', price: '800 DA', img: '../public/m1.png' },
-  { tag: 'EXTREME', title: 'Extreme Burger + Fries + Cola', price: '600 DA', img: '../public/m5.png' },
-  { tag: 'TENDER', title: '3 Pcs Chicken + Fries', price: '550 DA', img: '../public/m1.png' },
+  { tag: 'ZINGER', title: 'Zinger Poulet', price: '350 DA', img: '/m6.png' },
+  { tag: 'BROAST', title: '2 Pcs Chicken + Fries', price: '450 DA', img: '/m1.png' },
+  { tag: 'BROAST', title: '4 Pcs Chicken + Fries', price: '800 DA', img: '/m1.png' },
+  { tag: 'EXTREME', title: 'Extreme Burger + Fries + Cola', price: '600 DA', img: '/m5.png' },
+  { tag: 'TENDER', title: '3 Pcs Chicken + Fries', price: '550 DA', img: '/m1.png' },
 ]
 
 export const testimonials = [
@@ -22,10 +22,10 @@ export const testimonials = [
 ]
 
 export const gallery = [
-  '../public/1.jpg',
-  '../public/2.jpg',
-  '../public/3.jpg',
-  '../public/4.jpg',
-  '../public/5.png',
-  '../public/6.png',
+  '/public/1.jpg',
+  '/public/2.jpg',
+  '/public/3.jpg',
+  '/public/4.jpg',
+  '/public/5.png',
+  '/public/6.png',
 ]
